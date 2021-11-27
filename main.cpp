89bea9d9239e7428a8a6ec5b2593d4bc7540e383
Dennis H <dhilhorst2000@gmail.com>
2021-11-27 19:27:07 +0100

    add basic benchmark in main

--- main.cpp
+++ main.cpp
@@ -10,7 +10,17 @@ int main(int argc, char** argv) {
 
     frontend.Run();
 
-//    compute.FindSimplexDrawIndices<2>(1.6);
+//    auto fut = std::async(std::launch::async, &Compute<MAX_POINTS>::FindSimplexDrawIndices<3>, &compute, 0.6);
+//    auto start = std::chrono::steady_clock::now();
+//    while (fut.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
+//        std::printf("%d\n", compute.current_simplices);
+//        std::this_thread::sleep_for(std::chrono::milliseconds(60));
+//    }
+//    auto result = fut.get();
+//    auto duration = std::chrono::steady_clock::now() - start;
+//    std::printf("\n\n");
+//    std::printf("%lldms\n", std::chrono::duration_cast<std::chrono::milliseconds>(duration).count());
+//    std::printf("%llu simplices\n", result.size());
 
     return 0;
 }
